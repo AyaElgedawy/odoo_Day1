5e1c91c57b5058bd9cec280757d887bbca44f139
AyaElgedawy <ayaelgedawy4@gmail.com>
2024-03-06 03:45:18 +0200

    create todo_app

---
+++ todo_app/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "ToDo",
+    'summary': """""",
+    'description': """ """,
+    'author': "Aya Fathi",
+    'category': '',
+    'version': '17.0.0.1.0',
+    'depends': ['base',
+                ],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        'views/base_menues.xml',
+        'views/ticket.xml'
+    ],
+}
